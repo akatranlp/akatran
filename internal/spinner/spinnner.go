c32169439d27a5d382f949fda3183c7f57174fec
Fabian Petersen <fabian@nf-petersen.de>
2024-06-02 14:46:36 +0200

    spinner(fix): Move spinner to global and stop it after a signal
    When the programm is stopped while showing the signal the cursor is
    hidden an will not automatically show again.
    
    This is fixed with this issue

---
+++ internal/spinner/spinnner.go
@@ -0,0 +1,17 @@
+package spinner
+
+import (
+	"time"
+
+	"github.com/briandowns/spinner"
+)
+
+var s = spinner.New(spinner.CharSets[11], 100*time.Millisecond)
+
+func Start() {
+	s.Start()
+}
+
+func Stop() {
+	s.Stop()
+}
